f202bb664d26f9c234839ee25e083b22158bcc23
Andresnotch <andresnotch@gmail.com>
2024-03-17 13:58:56 -0700

    Add particles

--- roomie.gd
+++ roomie.gd
@@ -1,5 +1,5 @@
 extends Node2D
-@onready var animation = $AnimatedSprite2D
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -7,4 +7,4 @@ func _process(delta):
 	pass
 
 func _on_player_too_much_noise():
-	animation.play("default")
+	pass
